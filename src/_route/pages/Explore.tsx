8d60ab9a8affb509d2f534f32a11af637c61a2ed
kushalbhana <68599557+kushalbhana@users.noreply.github.com>
2024-03-03 13:03:10 +0530

    Grid Post andd Search results started

--- src/_route/pages/Explore.tsx
+++ src/_route/pages/Explore.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react"
 import { Input } from "@/components/ui/input";
+import GridPostList from "@/components/shared/GridPostList";
+import SearchResults from "@/components/shared/SearchResults";
 
 const Explore = () => {
   const [searchValue, setSearchValue] = useState('');
+
+  const posts = [];
+
+  const shouldShowSearchResults = searchValue !== '';
+  const shouldShowPosts = !shouldShowSearchResults && posts?.pages?.every((item) => item.document.length === 0)
 
   return (
     <div className="explore-container">
@@ -32,8 +39,18 @@ const Explore = () => {
         <div className="flex-between w-full max-w-5xl mt-16 mb-7">
           <h3 className="body-bold md:h3-bold">Popular Today</h3>
           <div className="flex-center gap-3 bg-dark-3 rouded-xl px-4 py-2 cursor-pointer">
-            <p className="small-medium">All</p>
+            <p className="small-medium md:base-medium text-light-2">All</p>
+            <img src="/assets/icons/filter.svg" alt="filter"
+            height={20}
+            width={20} />
           </div>
+        </div>
+        <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+            {shouldShowPosts ? (
+              <SearchResults />
+            ) : shouldShowPosts ?(
+              <p className="text-light-4 mt-10 text-center w-full"> End of Posts</p>
+            ): posts?.pages?.map((item, index) => ( <GridPostList key={`page-${index}`} posts={item.documents} />))}
         </div>
       </div>
   )
