d7f5d2946d99a6a93f7785962f99a639c561dabe
kushalbhana <68599557+kushalbhana@users.noreply.github.com>
2024-05-09 20:24:26 +0530

    made profile page responsive

--- src/_route/pages/Profile.tsx
+++ src/_route/pages/Profile.tsx
@@ -17,16 +17,16 @@ export default function Component() {
       <div className="explore-inner_container">
         <h2 className="h3-bold md:h2-bold w-full">Profile</h2>
         <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4 py-4">
-          <img src={user.imageUrl} alt="/assets/icons/profile-placeholder.svg" className="rounded-full h-36 w-36" />
+          <img src={user.imageUrl} alt="/assets/icons/profile-placeholder.svg" className="rounded-full h-20 w-20 md:h-36 md:w-36" />
           <div className="name">
             <div className="flex">
-            <h2 className="h2-bold mx-10">{user.name}</h2>
+            <h2 className=" h4-bold mx-6 md:h2-bold md:mx-10">{user.name}</h2>
             <img src="/assets/icons/edit.svg" height={20} width={20} alt="edit" className="align-right" />
             </div>
             
-            <h3 className="h3 mx-10">{`@${user.username}`}</h3>
+            <h3 className="mx-6 h4 md:mx-10">{`@${user.username}`}</h3>
             <div>
-              <p className="mx-10 mt-2 max-w-44">{user.bio}</p>
+              <p className="mx-6 md:mx-10 mt-2 max-w-44">{user.bio}</p>
             </div>
           </div>
 
@@ -38,7 +38,7 @@ export default function Component() {
         <hr className="border w-full border-dark-4/80" />
 
         <h3 className="body-bold md:h3-bold w-full my-10">
-          More Related Posts
+          Posts By User
         </h3>
         {isUserPostLoading || !relatedPosts ? (
           <Loader />
